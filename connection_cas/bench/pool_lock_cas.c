1386ea36754f72b07c5120da4e973d3b5709c7ef
boham97 <kangbosung97@gmail.com>
2025-06-21 16:36:32 +0900

    커넥션풀 동적으로 사이즈 수정하도록 변경

--- connection_cas/bench/pool_lock_cas.c
+++ connection_cas/bench/pool_lock_cas.c
@@ -4,11 +4,11 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-#define THREAD_CNT 40
-#define POOL_MIN_CNT 20
-#define POOL_MAX_CNT 20
-#define TRY 100000
-#define SLEEP 30000
+#define THREAD_CNT 30
+#define POOL_MIN_CNT 60
+#define POOL_MAX_CNT 60
+#define TRY 1000
+#define SLEEP 0
 
 
 //점유 플래그
@@ -24,9 +24,10 @@ pthread_key_t key;
 struct aligned_int 
 {
     int value;
-    long updated;                   //커넥션 생성 시간 -> 오래 돠면 정리후 재 연결
+    long updated;                   //커넥션 아이들 시작 시간 -> 오래 돠면 제거
+    long created;                   //커넥션 생성 시간 -> 오래되면 재 연결
     enum conn_flag flag;            //점유 상황
-    char padding[52];               // 64바이트 정렬
+    char padding[40];               // 64바이트 정렬
 } __attribute__((aligned(64)));
 
 int add_conn();
@@ -37,7 +38,7 @@ int is_valid(int i);
 
 void return_conn(int index);
 void return_conn_cas(int index);
-void conn_check(int i);
+
 pthread_mutex_t lock;               // 전역 뮤텍스
 
 int conn_lock_status[POOL_MAX_CNT] = {0,};
@@ -52,11 +53,44 @@ void *house_keeper(void *arg)
     int conn_count = 0;
     while(1)
     {
+        conn_count = 0;
         now = time(NULL);
         for (int i = 0; i < POOL_MAX_CNT; i++) 
         {
-            conn_check(i);
+            //1.아이들
+            if(__sync_bool_compare_and_swap(&conn_cas_status[i].value, 0, 1))
+            {
+                if(conn_count > POOL_MIN_CNT && conn_cas_status[i].updated + 10 * 60 < now)                 //아이들이 오래 됬다면 제거
+                {
+                    //대충 연결 해제
+                    printf("hk conn close\n");
+                    conn_cas_status[i].flag = CLOSED;
+                    conn_count--;
+                }
+                else
+                {
+                    conn_count++;
+                }
+                __sync_bool_compare_and_swap(&conn_cas_status[i].value, 1, 0);
+            }
+            //2. 닫힌 커넥션
+            else if (__sync_bool_compare_and_swap(&conn_cas_status[i].value, 1, 1) && conn_cas_status[i].flag == CLOSED)
+            {
+                //재연결
+                //printf("hk reconnect\n");
+                conn_cas_status[i].created = now;
+                conn_cas_status[i].updated = now;
+                conn_cas_status[i].flag = IN_USING;
+                __sync_bool_compare_and_swap(&conn_cas_status[i].value, 1, 0);
+            }
+            else
+            {
+                //printf("connect work\n ");
+                conn_count++;
+            }
+            //printf("hk %ld %ld\n", conn_cas_status[i].created, conn_cas_status[i].updated);
         }
+        //printf("conn cnt: %d\n\n", conn_count);
         sleep(1);
     }
     return NULL;
@@ -98,20 +132,19 @@ void *worker_cas(void *arg)
             if((res = get_conn_short()) != -1)
             {
                 cash++;
-                return_conn_cas(res);
-                break;;
+                break;
             }
             else if((res = get_conn_long())!= -1)
             {
-                return_conn_cas(res);
                 break;
             }
-            // else if((res = add_conn()) != -1)
-            // {
-            //     return_conn_cas(res);
-            //     break;
-            // }
+            else if((res = add_conn()) != -1)
+            {
+                break;
+            }
         }
+        usleep(10000);
+        return_conn_cas(res);
     }
     printf("%d %d\n", get, cash);
     return NULL;
@@ -137,12 +170,18 @@ int get_conn()
 
 int get_conn_long() 
 {
-    for (int i = 0; i < POOL_MAX_CNT; i++) 
+    int i = rand()%POOL_MAX_CNT;
+    for (int j = 0; j < POOL_MAX_CNT; j++) 
     {
+        i++;
+        if(i == POOL_MAX_CNT)
+            i = 0; 
         if(__sync_bool_compare_and_swap(&conn_cas_status[i].value, 0, 1)) 
         {
             pthread_setspecific(key, (void *)(intptr_t)i);
             conn_cas_status[i].flag = IN_USING;
+            conn_cas_status[i].updated = now;
+            //printf("%lu use %d\n", (unsigned long)pthread_self(), i);
             return i;
         }
     }
@@ -159,6 +198,8 @@ int get_conn_short()
         {
             pthread_setspecific(key, (void *)(intptr_t)index);
             conn_cas_status[index].flag = IN_USING;
+            conn_cas_status[index].updated = now;
+            //printf("%lu use short %d\n", (unsigned long)pthread_self(), index);
             return index;
         }
     }
@@ -168,13 +209,16 @@ int get_conn_short()
 int add_conn()
 {
     int i = 0;
-    for(i = POOL_MIN_CNT; i < POOL_MAX_CNT; i++)
+    for(i = 0; i < POOL_MAX_CNT; i++)
     {
         //대충 커넥션 만드는 로직 
         if(__sync_bool_compare_and_swap(&conn_cas_status[i].flag, CLOSED, IN_USING))
         {
             //언제 할당했는지 정보 필요
+            pthread_setspecific(key, (void *)(intptr_t)i);
             conn_cas_status[i].updated = now;       //매번 시간 계산 대신 하우스 키퍼 계산한 시간 사용
+            conn_cas_status[i].created = now;
+            printf("addconn deetected %d\n", i);
             return i;
         }
     }
@@ -193,7 +237,8 @@ void return_conn(int i)
 
 void return_conn_cas(int i)
 {
-    if(is_valid(i))
+    //반납때 오래됬으면 제거
+    if(is_valid(i) && conn_cas_status[i].created + 30 * 60 > now)
     {
         //커넥션이 멀정하면
         conn_cas_status[i].updated = now;
@@ -202,37 +247,15 @@ void return_conn_cas(int i)
     else
     {
         //커넥션 닫기
+        printf("%ld %ld conn close\n", now, conn_cas_status[i].created );
         conn_cas_status[i].flag = CLOSED;
     }
 }
 
-void conn_check(int i)
-{
-    //1. 아이들
-    if(__sync_bool_compare_and_swap(&conn_cas_status[i].value, 0, 1))
-    {
-        if(conn_cas_status[i].updated + 60 < now || is_valid(i) != 1)                 //오래됬거나 끊어 졌으면
-        {
-            //대충 재연결
-            conn_cas_status[i].updated = now;
-            conn_cas_status[i].flag = IN_USING;
-        }
-        __sync_bool_compare_and_swap(&conn_cas_status[i].value, 1, 0);
-    }
-    //2. 닫힌 커넥션
-    else if (__sync_bool_compare_and_swap(&conn_cas_status[i].value, 1, 1) && conn_cas_status[i].flag == CLOSED)
-    {
-        //재연결
-        conn_cas_status[i].updated = now;
-        conn_cas_status[i].flag = IN_USING;
-        __sync_bool_compare_and_swap(&conn_cas_status[i].value, 1, 0);
-    }
-
-}
 
 int main() 
 {
-    
+    srand(time(NULL));
     pthread_key_create(&key, NULL);
     struct timeval start, end;
     long elapsed;
@@ -242,18 +265,28 @@ int main()
     pthread_mutex_init(&lock, NULL); // 뮤텍스 초기화
     int i = 0;
 
+    gettimeofday(&start, NULL);
+    for(i =0; i < TRY; i++)
+        usleep(10000);
+
+    gettimeofday(&end, NULL);
+    
+    elapsed = (end.tv_sec - start.tv_sec) * 1000000L + (end.tv_usec - start.tv_usec);
+
+    printf("sleep 실행 시간: %ld 마이크로초 (%.3f초)\n", elapsed, elapsed / 1000000.0);
+
 
     gettimeofday(&start, NULL);
 
     for(i = 0; i< THREAD_CNT; i++)
     {
-        pthread_create(&thread_lock[i], NULL, worker_lock, NULL);
+        //pthread_create(&thread_lock[i], NULL, worker_lock, NULL);
     }
 
     
     for (i = 0; i < THREAD_CNT; i++) 
     {
-        pthread_join(thread_lock[i], NULL);
+        //pthread_join(thread_lock[i], NULL);
     }
 
     gettimeofday(&end, NULL);
@@ -262,11 +295,24 @@ int main()
 
     printf("MUTEX 실행 시간: %ld 마이크로초 (%.3f초)\n", elapsed, elapsed / 1000000.0);
 
-    // for(i = POOL_MIN_CNT; i < POOL_MAX_CNT; i++)
-    // {
-    //     conn_cas_status[i].value = 1;
-    //     conn_cas_status[i].flag = CLOSED;
-    // }
+    for(i = 0; i < POOL_MAX_CNT; i++)
+    {
+        if(i < POOL_MIN_CNT)
+        {
+            conn_cas_status[i].value = 0;
+            conn_cas_status[i].updated = time(NULL);
+            conn_cas_status[i].created = time(NULL);
+            conn_cas_status[i].flag = IN_USING;
+        }
+        else
+        {
+            conn_cas_status[i].value = 1;
+            conn_cas_status[i].updated = 0;
+            conn_cas_status[i].created = 0;
+            conn_cas_status[i].flag = CLOSED;            
+        }
+        //printf("%ld %ld\n", conn_cas_status[i].created, conn_cas_status[i].updated);
+    }
 
     pthread_create(&hk, NULL, house_keeper, NULL);
     gettimeofday(&start, NULL);
